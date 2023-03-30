dbbff5b2d9a53ba21be988af468903df524b6d09
Jiyun <grosjeanjiyun@icloud.com>
2023-03-30 09:41:27 +0200

    ortographe

--- src/utilities/formatCurrency.ts
+++ src/utilities/formatCurrency.ts
@@ -1,11 +1,11 @@
 // formatting price
-const CURRENCY_FORMATTET = new Intl.NumberFormat(undefined, {
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
     currency:"EUR",
     style:"currency",
 })
 
 const formatCurrency = (number:number) => {
-  return ( CURRENCY_FORMATTET.format(number) )
+  return ( CURRENCY_FORMATTER.format(number) )
 }
 
 export default formatCurrency
